b20463e987b5cd39c3422ac0a593d63610bac276
Robert Rees <shudderfix@gmail.com>
2016-07-22 23:05:27 +0100

    Punts the SSL enforcement earlier in the config

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ var users = require('./routes/users');
 
 var app = express();
 
+/**
+ * Enforce SSL
+ */
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,12 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
-/**
- * Enforce SSL
- */
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
