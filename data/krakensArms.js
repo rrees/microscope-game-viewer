cc5ad0f9dcec323031cf82f6279a0b39ba07f331
Robert Rees <shudderfix@gmail.com>
2016-07-28 21:44:18 +0100

    Creates a few of the periods to fill in the possibilities

--- data/krakensArms.js
+++ data/krakensArms.js
@@ -1,46 +1,91 @@
 
-const gameData = {
-	periods: [
+const theSignIsRaised = {
+	name: "The sign is raised",
+	tone: "light",
+	chronicle: {
+		focusCharacter: {
+			name: "Bart",
+			description: "Landlord and barman of the Kraken's Arms"
+		}
+	},
+	events: [
 		{
-			name: "Test start",
-			tone: "light",
-			chronicle: {
-				focusCharacter: {
-					name: "Joe",
-					description: "A person"
-				}
-			},
-			events: [
-				{
-					name: "First event",
-					tone: "dark",
-					scenes: [
-						{
-							question: "Why is this here?",
-							answer: "As an example",
-							description: "Optional",
-							tone: "light",
-							characters: [
-								{
-									name: "Blah",
-									description: "Optional"
-								}
-							],
-							exclusions: []
-						}
-					]
-				}
-
-			]
+			name: "The sign appears from nowhere",
+			tone: "light"
 		},
 		{
-			name: "Test end",
-			tone: "dark",
-			events: [
-
+			name: "The Kraken's Arms gains popularity",
+			tone: "light"
+		},
+		{
+			name: "The first magical visitors arrive at the bar",
+			tone: "light",
+			scenes: [
+				{
+					question: "Who was the first magical visitor?",
+					answer: "The leaders of the Undivided Circle",
+					description: "Optional",
+					tone: "light",
+					characters: [
+						{
+							name: "Blah",
+							description: "Optional"
+						}
+					],
+					exclusions: []
+				}
 			]
 		}
 
+	]
+};
+
+const theGreatDivide = {
+	name: "The Great Divide",
+	tone: "dark",
+	chronicle: {
+		focusCharacter: {
+			name: "Sir Fairchild Humphries",
+			description: "Owner of the Arms, master wizard and dandy whose looks never fade"
+		}
+	},
+	events: [
+		{
+			name: "Black-Tooth Jack meets Sir Fairchild Humphries",
+			tone: "light"
+		}
+	]
+};
+
+const theMagicDies = {
+	name: "The magic dies",
+	tone: "dark",
+	chronicle: {
+		focusCharacter: {
+			name: "Donald Trump",
+			description: "Small handed vulgarian with stupid hair"
+		}
+	},
+	events: [
+		{
+			name: "Trump buys the land on which the pub is built",
+			tone: "dark",
+			scenes: [
+				{
+					question: "How does he manage this?",
+					answer: "The city officials are bullied into submission and deported (for looking Mexican... maybe)",
+					tone: "dark"
+				}
+			]
+		}
+	]
+};
+
+const gameData = {
+	periods: [
+		theSignIsRaised,
+		theGreatDivide,
+		theMagicDies
 	]
 };
 
